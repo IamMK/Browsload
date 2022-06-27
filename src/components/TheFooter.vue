eb3130bdf1aa8cc25779dda03f565e00ac0d047b
IamMK <malk@int.pl>
2022-06-27 17:56:35 +0200

    Added Main and Footer Compoents

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+    <p class="footer__paragraph">Made by MK&copy; 2022</p>
+  </footer>
+</template>
+
+<style lang="scss">
+.footer {
+  text-align: center;
+  font-size: 24px;
+  &__paragraph {
+    margin: 0;
+  }
+}
+
+@media all and (max-width: 1000px) {
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+}
+
+@media all and (max-width: 500px) {
+  .footer {
+    font-size: 16px;
+  }
+}
+</style>
--- src/store/app/index.ts
+++ src/store/app/index.ts
@@ -1,7 +1,7 @@
 export const appStore = {
   state: () => {
     return {
-      siteurl: "/order/",
+      siteUrl: "/orders/",
       orderNo: "",
       error: false,
       label: "order.zip",
